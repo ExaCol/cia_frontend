4fa9af675c24fd0fa1dffb6b5ec92f23b06bd2a1
Chakerr <luisgas012@gmail.com>
2025-10-01 06:59:14 -0500

    add

--- src/app/client/page.tsx
+++ src/app/client/page.tsx
@@ -5,12 +5,22 @@ Usuario Home Page
 */
 
 import "@/styles/globals.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div>
       <h2 style = {{textAlign : "center"}}>Ésta es la página de Cliente de SmartTraffic</h2>
-      <button>Usuario</button>
+      <div>
+        <Link href="/client/services">
+          <button>Mis servicios</button>
+        </Link>
+
+        <Link href="/client/services/new">
+          <button>Solicitar servicio</button>
+        </Link>
+      </div>
     </div>
+    
   );
 }
